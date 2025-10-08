51d163038657516acf2addccdb83c835b7f2704b
Chase Douglas <chasedouglas@gmail.com>
2025-10-08 18:45:41 +0000

    Add task to launch SurrealDB CLI for self-hosted DB

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -2,3 +2,6 @@
 
 # Fix ownership of volume mounted directories
 sudo chown vscode:vscode /home/vscode/.aws /usr/local/cargo/registry /usr/local/cargo/git /workspaces/archodex-backend/target
+
+# Install / update SurrealDB CLI
+curl -sSf https://install.surrealdb.com | sudo sh

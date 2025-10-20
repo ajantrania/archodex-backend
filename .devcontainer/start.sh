e250660d661a30777c6ba128d7c8b21abc80d7e7
Apurva Jantrania <jmareel@gmail.com>
2025-10-20 14:16:03 +0900

    Improve Devcontainer Config
    * Fix volume ownership
    * Seperate backend cargo registry cache
    * Enable port forwarding for local agent<->backend workflow

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 # Fix ownership of volume mounted directories
-sudo chown vscode:vscode /home/vscode/.aws /usr/local/cargo/registry /usr/local/cargo/git /workspaces/archodex-backend/target
+sudo chown -R vscode:vscode /home/vscode/.aws /usr/local/cargo/registry /usr/local/cargo/git /usr/local/rustup /workspaces/archodex-backend/target
 
 # Install / update SurrealDB CLI
 curl -sSf https://install.surrealdb.com | sudo sh
